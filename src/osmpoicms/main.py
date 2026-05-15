1b3c8907a21e28003c69559dc3e40887efc50279
Anton Pirker <anton@ignaz.at>
2026-05-15 09:44:23 +0200

    Fix dev start script using project.scripts entry point

--- src/osmpoicms/main.py
+++ src/osmpoicms/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from osmpoicms.routes import auth, index
@@ -5,3 +6,12 @@ from osmpoicms.routes import auth, index
 app = FastAPI(title="OSM POI CMS")
 app.include_router(auth.router)
 app.include_router(index.router)
+
+
+def dev() -> None:
+    uvicorn.run(
+        "osmpoicms.main:app",
+        reload=True,
+        ssl_keyfile="localhost-key.pem",
+        ssl_certfile="localhost.pem",
+    )
